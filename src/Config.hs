1c255928bff466646cb42218234edf9f77b9f2f0
Eugene <eugene.naumenko@gmail.com>
2016-02-21 19:23:49 +0200

    Cleanup

--- src/Config.hs
+++ src/Config.hs
@@ -1,11 +1,6 @@
 module Config where
 
 serverName = "api.twitter.com"
-oauthConsumerKey = "C2dfbPN6OmE9fsGHXbEEkOYmf"
-oauthConsumerSecret = "uYy749NZY0lCzQro8mH1joeYglnzjGzfZEWVd8jVArsKTxnHZa"
-
--- accessToken = "897203563-rFrcObucJd4KByiOH9YSfoSYZFvwZ3DepAX6I0lF"
--- accessTokenSecret = "xe8EWIm79jw6uSBKWbKQWUGHukb8PHzaxCcg2OOrNxwuY"
 
 oneSecond = 1000000 :: Int
 oneMinute = 60 * oneSecond
@@ -21,5 +16,3 @@ timeoutWorkerDelay = (timeoutThreshod * oneSecond) - oneSecond
 updateRetryCount = 30 :: Int
 updateRetryDelay = 2 * oneSecond
 updateFeedAsync = False
-
--- cloudDbUrl = "https://sweltering-heat-9224.firebaseio.com/.json" :: String
